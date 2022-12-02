7b92456df85148aa0813fa4cd94ad7f87a3c0686
LanoCodes <del.pow1@gmail.com>
2022-12-02 04:51:07 -0500

    - main code cleanup and improved functionality for display, README further developed

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # NATO-Phonetic-Translator
+This simple project gets a word as an input and then translates it into a phonetic representation of itself, using list comprehensions to do so.
+
+![img_1.png](img_1.png)
+
+## Installation
+1. Create a directory on your machine to house it. I find "TRANSLATE" to work just fine!
+```bash 
+mkdir TRANSLATE
+```
+2. Look above and select "Code"
+3. From here, copy the HTTPS web URL
+4. Return to your terminal and clone the project.
+```bash
+git clone https://github.com/LanoCodes/NATO-Phonetic-Translator.git
+```
+5. Using your favorite IDE, open the TRANSLATE folder
+6. Navigate to main and run from there. You will need to have the pandas package installed.
+
+## Usage
+- When prompted, enter a word and then press enter. In my case above, I entered a name.
+- After that, you'll be shown what a phonetic translation of your word looks like.
--- main.py
+++ main.py
@@ -1,31 +1,9 @@
-student_dict = {
-    "student": ["Angela", "James", "Lily"], 
-    "score": [56, 76, 98]
-}
-
-#Looping through dictionaries:
-for (key, value) in student_dict.items():
-    #Access key and value
-    pass
-
 import pandas as pd
-student_data_frame = pd.DataFrame(student_dict)
 
-#Loop through rows of a data frame
-for (index, row) in student_data_frame.iterrows():
-    #Access index and row
-    #Access row.student or row.score
-    pass
-
-# Keyword Method with iterrows()
-# {new_key:new_value for (index, row) in df.iterrows()}
-
-#TODO 1. Create a dictionary in this format:
-# {"A": "Alfa", "B": "Bravo"}
 nato_phon_alph_DataFrame = pd.read_csv("nato_phonetic_alphabet.csv")
 nato_phon_alph_dict = {row.letter:row.code for (index, row) in nato_phon_alph_DataFrame.iterrows()}
 
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 user_word = input("Enter word for phonetic translation: ").upper()
 phonetic_translation = [nato_phon_alph_dict[letter] for letter in user_word]
-print(phonetic_translation)
+for letter in phonetic_translation:
+    print(letter)
