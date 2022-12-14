cd8b356e8a9fdaceed406cf833be53b3a38aa9f8
LanoCodes <del.pow1@gmail.com>
2022-12-14 12:29:39 -0500

    - Error handling for KeyError implemented

--- main.py
+++ main.py
@@ -3,7 +3,16 @@ import pandas as pd
 nato_phon_alph_DataFrame = pd.read_csv("nato_phonetic_alphabet.csv")
 nato_phon_alph_dict = {row.letter:row.code for (index, row) in nato_phon_alph_DataFrame.iterrows()}
 
-user_word = input("Enter word for phonetic translation: ").upper()
-phonetic_translation = [nato_phon_alph_dict[letter] for letter in user_word]
-for letter in phonetic_translation:
-    print(letter)
+
+def translate():
+    user_word = input("Enter word for phonetic translation: ").upper()
+    try:
+        phonetic_translation = [nato_phon_alph_dict[letter] for letter in user_word]
+    except KeyError:
+        print("Only enter letters in the alphabet please.")
+        translate()
+    else:
+        for letter in phonetic_translation:
+            print(letter)
+
+translate()
